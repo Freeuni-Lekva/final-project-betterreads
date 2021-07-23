40663fee122c024b34e9dc1aa544f42f3abec7a3
unknown <tjije19@freeuni.edu.ge>
2021-07-23 14:30:55 +0400

    Fill Tables

---
+++ FillDB/book-genres.sql
@@ -0,0 +1,45 @@
+
+insert into book_genres(book_id, genre_id)
+           values(1, 21);
+           
+insert into book_genres(book_id, genre_id)
+           values(2, 21);
+           
+insert into book_genres(book_id, genre_id)
+           values(3, 21);
+
+insert into book_genres(book_id, genre_id)
+           values(4, 22);
+
+insert into book_genres(book_id, genre_id)
+           values(4, 5);
+
+insert into book_genres(book_id, genre_id)
+           values(5, 5);
+
+insert into book_genres(book_id, genre_id)
+           values(5, 7);
+
+insert into book_genres(book_id, genre_id)
+           values(6, 23);
+
+insert into book_genres(book_id, genre_id)
+           values(7, 22);
+
+insert into book_genres(book_id, genre_id)
+           values(7, 21);
+
+insert into book_genres(book_id, genre_id)
+           values(7, 4);
+
+insert into book_genres(book_id, genre_id)
+           values(8, 5);
+
+insert into book_genres(book_id, genre_id)
+           values(9, 10);
+
+insert into book_genres(book_id, genre_id)
+           values(9, 24);
+
+insert into book_genres(book_id, genre_id)
+           values(10, 24);
