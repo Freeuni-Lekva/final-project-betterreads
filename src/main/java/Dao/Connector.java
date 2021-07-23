84662230994631dd3834dfea4c8eb8557c6586a1
ninikhundzakishvili <nkhun19@freeuni.edu.ge>
2021-07-23 16:25:14 +0400

    create connection for database

--- src/main/java/Dao/Connector.java
+++ src/main/java/Dao/Connector.java
@@ -1,6 +1,24 @@
 package Dao;
 
-public abstract class Connector {
+import org.apache.commons.dbcp.BasicDataSource;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 
+public class Connector {
+    private static Connection connection;
+    private Connector(String dbName) throws SQLException {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/library");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        connection = dataSource.getConnection();
+    }
+
+    public static Connection getConnection(String dbName) throws SQLException {
+        if(connection == null){
+            new Connector(dbName);
+        }
+        return connection;
+    }
 }
