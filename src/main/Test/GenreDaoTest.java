944fcd0ec2fbcf1f008556b42f98fd3c8d9b9920
ninikhundzakishvili <nkhun19@freeuni.edu.ge>
2021-07-24 20:59:44 +0400

    add test class for GenreDao

---
+++ src/main/Test/GenreDaoTest.java
@@ -0,0 +1,42 @@
+import Dao.Connector;
+import Dao.GenreDao;
+import Model.Genre;
+import junit.framework.TestCase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GenreDaoTest extends TestCase {
+    private Connection connection;
+    private GenreDao genreDao;
+
+    @Override
+    protected void setUp() throws SQLException {
+        connection = Connector.getConnection("testLibrary");
+        genreDao = new GenreDao("testLibrary");
+    }
+
+    public void testGetGenre() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into genres(genre_name)" +
+                " values (?);");
+        preparedStatement.setString(1, "horori");
+        preparedStatement.execute();
+        assertEquals(1, genreDao.getGenreById(1).getGenre_id());
+        assertEquals("horori", genreDao.getGenreById(1).getGenre_name());
+    }
+
+    public void testAddGenre() throws SQLException {
+        Genre newGenre = new Genre();
+        newGenre.setGenre_name("varchar2");
+        genreDao.addGenre(newGenre);
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from genres " +
+                                                                                "where genre_name = ?");
+        preparedStatement.setString(1, "varchar2");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            assertEquals("varchar2", resultSet.getString("genre_name"));
+        }
+    }
+}
