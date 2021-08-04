c2c094e6d89f884c027075e9acd9ef0dff839b43
EleneTs <etsir19@freeuni.edu.ge>
2021-08-04 15:00:37 +0400

    logout button and no links when user is logged in

--- src/main/java/Listeners/WebAppListener.java
+++ src/main/java/Listeners/WebAppListener.java
@@ -6,9 +6,12 @@ import Service.AllServices;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 @WebListener
-public class WebAppListener implements ServletContextListener {
+public class WebAppListener implements ServletContextListener, HttpSessionListener {
     private AllServices allServices;
     public WebAppListener(){
         allServices = new AllServices();
@@ -21,5 +24,16 @@ public class WebAppListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         servletContextEvent.getServletContext().removeAttribute(SharedConstants.ATTRIBUTE);
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+        HttpSession session = httpSessionEvent.getSession();
+        session.setAttribute(SharedConstants.SESSION_ATTRIBUTE, null);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        httpSessionEvent.getSession().removeAttribute(SharedConstants.SESSION_ATTRIBUTE);
     }
 }
---
+++ src/main/java/Servlets/LogoutServlet.java
@@ -0,0 +1,18 @@
+package Servlets;
+
+import Constants.SharedConstants;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().removeAttribute(SharedConstants.SESSION_ATTRIBUTE);
+        request.getRequestDispatcher("WEB-INF/HomePage.jsp").forward(request,response);
+    }
+}
