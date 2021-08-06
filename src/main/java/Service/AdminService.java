9b44c993663667fdcb99d108e11050ab832c8c62
EleneTs <etsir19@freeuni.edu.ge>
2021-08-07 03:51:31 +0400

    updated adminService

--- src/main/java/Service/AdminService.java
+++ src/main/java/Service/AdminService.java
@@ -2,16 +2,21 @@ package Service;
 
 import Constants.SharedConstants;
 import Dao.AdminDao;
+import Dao.AuthorDao;
 import Dao.Connector;
+import Model.Author;
+import Model.Book;
 import Model.User;
 
 import java.sql.SQLException;
 
 public class AdminService implements AdminServiceInterface{
     private AdminDao adminDao;
+    private AuthorDao authorDao;
     public AdminService(){
         try {
             adminDao = new AdminDao(Connector.getConnection(SharedConstants.DATA_BASE_NAME));
+            authorDao = new AuthorDao(Connector.getConnection(SharedConstants.DATA_BASE_NAME));
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -19,5 +24,24 @@ public class AdminService implements AdminServiceInterface{
     @Override
     public boolean isAdmin(User user) {
         return adminDao.isAdmin(user);
+    }
+
+    @Override
+    public Author getAuthorByName(String author_name) {
+        return authorDao.getAuthorByName(author_name);
+    }
+
+    @Override
+    public boolean addBook(String book_name, Author author, int release_year, int count, String photo, String description, String[] genres) {
+        Book newBook = new Book();
+        newBook.setBook_name(book_name);
+        newBook.setAuthor_id(author.getAuthor_id());
+        newBook.setRelease_year(release_year);
+        newBook.setBook_rating(0);
+        newBook.setAvailable_count(count);
+        newBook.setBook_description(description);
+        newBook.setBook_photo(photo);
+        return adminDao.addBook(newBook,genres);
+
     }
 }
--- src/main/java/Service/AdminServiceInterface.java
+++ src/main/java/Service/AdminServiceInterface.java
@@ -1,12 +1,15 @@
 package Service;
 
+import Model.Author;
 import Model.User;
 
 import java.util.List;
 
 public interface AdminServiceInterface {
 
-    boolean isAdmin(User user);
+    public boolean isAdmin(User user);
 
+    public Author getAuthorByName(String author_name);
 
+    public boolean addBook(String book_name,Author author,int release_year,int count, String photo, String description, String[] genres);
 }
