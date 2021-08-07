c3be5e186709af69d8f61f2a6187f0a7786a4a39
ninikhundzakishvili <nkhun19@freeuni.edu.ge>
2021-08-08 01:18:57 +0400

    added methods like/unlike/delete review

--- src/main/java/Service/ReviewService.java
+++ src/main/java/Service/ReviewService.java
@@ -11,26 +11,42 @@ import java.util.List;
 public class ReviewService implements ReviewServiceInterface{
     private ReviewDao reviewDao;
 
-    public ReviewService(){
-        try {
-            reviewDao = new ReviewDao(Connector.getConnection(SharedConstants.DATA_BASE_NAME));
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    public ReviewService() throws SQLException {
+        reviewDao = new ReviewDao(Connector.getConnection(SharedConstants.DATA_BASE_NAME));
     }
 
     @Override
-    public List<Review> getReviewsByBookId(int book_id) {
+    public List<Review> getReviewsByBookId(int book_id) throws SQLException {
         return reviewDao.getReviews(book_id);
     }
 
     @Override
-    public List<Review> getReviewsByBookIdUserId(int book_id, int user_id) {
+    public List<Review> getReviewsByBookIdUserId(int book_id, int user_id) throws SQLException {
         return reviewDao.getReviewsByUserId(book_id, user_id);
     }
 
     @Override
-    public void addReview(int user_id, int book_id, String comment, String date, int num_likes) {
+    public void addReview(int user_id, int book_id, String comment, String date, int num_likes) throws SQLException {
         reviewDao.addReview(user_id, book_id, comment, date, num_likes);
+    }
+
+    @Override
+    public boolean hasUserLikedAlready(int review_id, int user_id) throws SQLException {
+        return reviewDao.alreadyLiked(user_id, review_id);
+    }
+
+    @Override
+    public boolean likeReview(int user_id, int review_id) throws SQLException {
+        return reviewDao.likeReview(user_id, review_id);
+    }
+
+    @Override
+    public boolean unlikeReview(int user_id, int review_id) throws SQLException {
+        return reviewDao.unlikeReview(user_id, review_id);
+    }
+
+    @Override
+    public boolean deleteReview(int review_id) throws SQLException {
+        return reviewDao.deleteReview(review_id);
     }
 }
