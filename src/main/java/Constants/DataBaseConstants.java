2819fdb0676977d8eaa0b5b7fa9d730c43aa7e30
bgame19 <82844627+bgame19@users.noreply.github.com>
2021-08-07 15:08:28 +0400

    Update DataBaseConstants.java

--- src/main/java/Constants/DataBaseConstants.java
+++ src/main/java/Constants/DataBaseConstants.java
@@ -2,6 +2,6 @@ package Constants;
 
 public class DataBaseConstants {
     public static final String DATA_BASE_USERNAME = "root";
-    public static final String DATA_BASE_PASSWORD = "pass1234";
+    public static final String DATA_BASE_PASSWORD = "root";
     public static final String DATA_BASE_URL = "jdbc:mysql://localhost:3306/";
 }
